e398d155a1d0f14f215c200af23e1d5b8b01263d
nicholasbair <bairinc@gmail.com>
2017-06-27 18:13:24 -0400

    More controller action stubbing.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,19 @@
 class ListsController < ApplicationController
   def index
+  end
 
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,19 @@
 class TasksController < ApplicationController
   def index
-    
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
   end
 end
