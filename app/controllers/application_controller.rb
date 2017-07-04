90f0d59037edadc8580b69f1ba8ef43003906ffe
nicholasbair <bairinc@gmail.com>
2017-07-04 17:03:17 -0400

    Redirect to user/:id/lists after signin, closes #25.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  def after_sign_in_path_for(resource)
+    user_lists_path(current_user)
+  end
+
   private
 
     def user_not_authorized
