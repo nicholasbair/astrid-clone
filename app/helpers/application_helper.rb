c453132c8a86769af37ed0ccef1175a318921fe9
nicholasbair <bairinc@gmail.com>
2017-07-05 13:06:22 -0400

    Refactor forms, create form wrapper helper for common bootstrap elements.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def form_wrapper
+    content_tag :div, :class => 'row' do
+      content_tag :div, :class => 'col-md-6 col-md-offset-3 form-card' do
+        yield
+      end
+    end
+  end
 end
