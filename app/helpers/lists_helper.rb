89dde56db50cff231a026754714fb441b75f5da8
nicholasbair <bairinc@gmail.com>
2017-07-06 14:26:54 -0400

    Work on task/list views.

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -4,6 +4,8 @@ module ListsHelper
   end
 
   def render_add_task(f)
-    render :partial => "add_task", :locals => { :f => f } if current_page?(:action => "new")
+    if current_page?(:action => "new") || current_page?(:action => "show")
+      render :partial => "add_task", :locals => { :f => f }
+    end
   end
 end
