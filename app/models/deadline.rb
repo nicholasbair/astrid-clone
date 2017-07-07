544c54bd4661cea62d2b563ec108b1fe8bfdf77d
nicholasbair <bairinc@gmail.com>
2017-07-07 18:47:14 -0400

    WIP #42, add twilio.

--- app/models/deadline.rb
+++ app/models/deadline.rb
@@ -3,4 +3,26 @@ class Deadline < ActiveRecord::Base
 
   belongs_to :list
   has_one :user, :through => :list
+
+  after_create :reminder
+
+  @@REMINDER_TIME = 1.day
+
+  def reminder
+    @twilio_number = ENV['TWILIO_NUMBER']
+    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+    reminder = "Hi #{self.user.username}. Just a reminder, #{self.list.title} is incomplete and due tomorrow."
+    message = @client.account.messages.create(
+      :from => @twilio_number,
+      :to => self.phone_number,
+      :body => reminder,
+    )
+    puts message.to
+  end
+
+  def when_to_run
+    date - @@REMINDER_TIME
+  end
+
+  handle_asynchronously :reminder, :run_at => Proc.new { |i| i.when_to_run }
 end
