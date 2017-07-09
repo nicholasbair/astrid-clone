87aba076d7a5ea7ad78bb697095408ec32a4756a
nicholasbair <bairinc@gmail.com>
2017-07-09 16:17:37 -0400

    WIP #3, ominauth almost done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and
-  devise :omniauthable, :omniauth_providers => [:github]
-  devise :database_authenticatable, :registerable, #:omniauthable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+    :trackable, :validatable, :omniauthable, :omniauth_providers => [:github]
 
   has_many :lists
   has_many :deadlines, :through => :lists
   has_many :tasks, :through => :lists
 
-  validates :phone_number, :presence => true
-  validates :time_zone, :presence => true
+  # NOTE: this may cause omniauth signup to fail
+  # validates :phone_number, :presence => true
+  # validates :time_zone, :presence => true
 
   def self.most_completed_tasks
     results = self.joins(:lists).where(lists: { status: "complete" }).limit(5).group("lists.user_id").count
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.username = auth.info.name   # assuming the user model has a name
+      # user.username = auth.info.name   # assuming the user model has a name
       # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
