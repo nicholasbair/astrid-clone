1e560dcb2932d9aa84ac8a48fd38cc21c33017e9
nicholasbair <bairinc@gmail.com>
2017-07-09 13:19:00 -0400

    Over ride devise reg controller for additional params.

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,13 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+
+  private
+
+    def sign_up_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :phone_number, :time_zone)
+    end
+
+    def account_update_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :phone_number, :time_zone)
+    end
+
+end
