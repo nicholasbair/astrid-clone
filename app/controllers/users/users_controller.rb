dcd6718a4057fa2bbabd3e2d782c8cdb04440d2b
nicholasbair <bairinc@gmail.com>
2017-07-11 11:31:36 -0400

    Clean up routes, closes #59.

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,4 +1,7 @@
 class Users::UsersController < ApplicationController
+  # before_filter :authenticate_user!
+  # before_filter :authorize_user!
+
   def most_completed_tasks
     @most_completed = User.most_completed_tasks
   end
