9d8c7ed6b8c00e1e5b4d323aa30f606eced9fa1f
nicholasbair <bairinc@gmail.com>
2017-07-11 13:54:22 -0400

    Add reminder interval w/ validations, closes #58.

--- app/models/deadline.rb
+++ app/models/deadline.rb
@@ -2,6 +2,7 @@ class Deadline < ActiveRecord::Base
   validates :time, :presence => true
   validates :reminder_interval, :presence => true
   validate :time_must_be_in_the_future
+  validate :reminder_must_be_in_the_future
 
   belongs_to :list
   has_one :user, :through => :list
@@ -12,18 +13,22 @@ class Deadline < ActiveRecord::Base
     errors.add(:time, "must be in the future.") unless Time.now < time
   end
 
+  def reminder_must_be_in_the_future
+    errors.add(:reminder_interval, "must be in the future.") unless Time.now < (time - reminder_interval.minutes)
+  end
+
   def reminder
-    # if !self.list.complete?
-    #   @twilio_number = ENV['TWILIO_NUMBER']
-    #   @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
-    #   reminder = "Hi. Just a reminder, your list #{self.list.title} is incomplete and due in #{reminder_interval.to_s} minutes"
-    #   message = @client.messages.create(
-    #     :from => @twilio_number,
-    #     :to => "1#{self.user.phone_number}",
-    #     :body => reminder,
-    #   )
-    #   puts message.to
-    # end
+    if !self.list.complete?
+      @twilio_number = ENV['TWILIO_NUMBER']
+      @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+      reminder = "Hi. Just a reminder, your list #{self.list.title} is incomplete and due in #{reminder_interval.to_s} minutes"
+      message = @client.messages.create(
+        :from => @twilio_number,
+        :to => "1#{self.user.phone_number}",
+        :body => reminder,
+      )
+      puts message.to
+    end
   end
 
   def when_to_run
