571164c15ad6b8f3110301f5fa74ac5b67675a49
nicholasbair <bairinc@gmail.com>
2017-07-11 12:43:12 -0400

    Add phone number validation using Twilio, closes #56.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
     :trackable, :validatable, :omniauthable, :omniauth_providers => [:github]
 
@@ -8,9 +6,24 @@ class User < ActiveRecord::Base
   has_many :deadlines, :through => :lists
   has_many :tasks, :through => :lists
 
-  # NOTE: this may cause omniauth signup to fail
-  # validates :phone_number, :presence => true
-  # validates :time_zone, :presence => true
+  validates :phone_number, :uniqueness => true
+  validate :phone_number_must_be_valid, unless: Proc.new { |a| a.phone_number.blank? }
+
+  def phone_number_must_be_valid
+    client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']).lookups
+    begin
+      response = client.v1.phone_numbers(phone_number).fetch(type: 'carrier')
+      if response.carrier['type'] != 'mobile'
+        errors.add(:phone_number, "must be able to recieve text messages")
+      end
+    rescue => e
+      if e.code == 20404
+        errors.add(:phone_number, "must be valid")
+      else
+        raise e
+      end
+    end
+  end
 
   def self.most_completed_tasks
     results = self.joins(:lists).where(lists: { status: "complete" }).limit(5).group("lists.user_id").count
@@ -27,8 +40,6 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      # user.username = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
